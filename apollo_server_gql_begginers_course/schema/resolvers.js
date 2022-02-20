595d10d38a5a9f4a1ad6b153fd9bffd306ba52ce
vv_tomilin <backatom1@gmail.com>
2022-02-20 12:55:13 +0500

    Finished the 4rd lesson <Resolvers>

--- apollo_server_gql_begginers_course/schema/resolvers.js
+++ apollo_server_gql_begginers_course/schema/resolvers.js
@@ -1,11 +1,42 @@
-const { UserList } = require('../FakeData');
+const _ = require('lodash');
+
+const { UserList, MovieList } = require('../FakeData');
 
 const resolvers = {
   Query: {
-    users() {
+
+    //* USER RESOLVERS
+    users: () => {
       return UserList;
+    },
+    user: (parent, args) => {
+      const id = args.id;
+      const user = _.find(UserList, { id: Number(id) });
+      return user;
+    },
+
+    //* MOVIE RESOLVERS
+    movies: () => {
+      return MovieList;
+    },
+    movie: (parent, args) => {
+      const name = args.name;
+      const movie = _.find(MovieList, { name: name })
+      return movie;
     }
-  }
+  },
+
+  User: {
+    favoriteMovies: () => {
+      return _.filter(MovieList, (movie) => {
+        return (
+          movie.yearOfPublication >= 2000
+          &&
+          movie.yearOfPublication <= 2010
+        );
+      });
+    }
+  },
 };
 
 module.exports = { resolvers };
--- apollo_server_gql_begginers_course/schema/type-defs.js
+++ apollo_server_gql_begginers_course/schema/type-defs.js
@@ -6,11 +6,31 @@ const typeDefs = gql`
     name: String!
     username: String!
     age: Int!
-    nationality: String!
+    nationality: Nationality!
+    friends: [User]
+    favoriteMovies: [Movie]
+  }
+
+  type Movie {
+    id: ID!
+    name: String!
+    yearOfPublication: Int!
+    isInTheaters: Boolean!
   }
 
   type Query {
     users: [User!]!
+    user(id: ID!): User!
+    movies: [Movie!]!
+    movie(name: String!): Movie!
+  }
+
+  enum Nationality {
+    CANADA
+    BRAZIL
+    INDIA
+    GERMANY
+    CHILE
   }
 `;
 
